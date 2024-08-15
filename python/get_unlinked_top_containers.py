4bb3568f4e6d18a6ef7103ebafd8ddac22579ac5
Zoe Tucker <zoetucker@library.ucla.edu>
2024-08-15 10:32:09 -0700

    SYS-1665: script to find unlinked top containers

---
+++ python/get_unlinked_top_containers.py
@@ -0,0 +1,39 @@
+import asnake.logging as logging
+from asnake.client import ASnakeClient
+
+logging.setup_logging(filename="archivessnake.log", level="INFO")
+# set label for custom logger - all output will be in archivessnake.log
+logger = logging.get_logger("get_unlinked_top_containers")
+client = ASnakeClient()
+
+
+def get_unlinked_top_containers():
+    all_top_containers = client.get(
+        "repositories/2/top_containers", params={"all_ids": True}
+    ).json()
+    total_top_containers = len(all_top_containers)
+    logger.info(f"Total top containers: {total_top_containers}")
+    pages = (total_top_containers // 10) + 1
+    logger.info(f"Total pages: {pages}")
+
+    output_list = []
+    for i in range(1, pages + 1):
+        logger.info(f"Page: {i}")
+        top_containers = client.get(
+            "repositories/2/top_containers", params={"page": i}
+        ).json()
+        for top_container in top_containers["results"]:
+            # unlinked top containers have an empty collection field
+            if len(top_container["collection"]) == 0:
+                logger.info(f"Unlinked top container: {top_container['uri']}")
+                output_list.append(top_container["uri"])
+
+    logger.info(f"Total unlinked top containers: {len(output_list)}")
+    with open("unlinked_top_containers.txt", "w") as f:
+        for item in output_list:
+            f.write(f"{item}\n")
+    logger.info("Output written to unlinked_top_containers.txt")
+
+
+if __name__ == "__main__":
+    get_unlinked_top_containers()
