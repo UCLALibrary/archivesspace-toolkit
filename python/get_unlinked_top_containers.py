11ceaea535a399755e2875c717b57afc650d4ca2
Zoe Tucker <zoetucker@library.ucla.edu>
2024-08-16 10:08:58 -0700

    SYS-1665: Script to find unlinked top containers for deletion (#5)
    * use get_paged() and page size argument
    * use int type in arg parser, update gitignore
    * fix page_size help text
    * fix gitignore formatting

---
+++ python/get_unlinked_top_containers.py
@@ -0,0 +1,39 @@
+import asnake.logging as logging
+from asnake.client import ASnakeClient
+import argparse
+
+logging.setup_logging(filename="archivessnake.log", level="INFO")
+# set label for custom logger - all output will be in archivessnake.log
+logger = logging.get_logger("get_unlinked_top_containers")
+client = ASnakeClient()
+
+
+def get_unlinked_top_containers(page_size: int = 250):
+
+    output_list = []
+    for top_container in client.get_paged(
+        "repositories/2/top_containers", page_size=page_size
+    ):
+        # unlinked top containers have an empty collection field
+        if len(top_container["collection"]) == 0:
+            logger.info(f"Unlinked top container: {top_container['uri']}")
+            output_list.append(top_container["uri"])
+
+    logger.info(f"Total unlinked top containers: {len(output_list)}")
+    with open("unlinked_top_containers.txt", "w") as f:
+        for item in output_list:
+            f.write(f"{item}\n")
+    logger.info("Output written to unlinked_top_containers.txt")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--page_size",
+        help="Number of records to retrieve per page",
+        default=250,
+        type=int,
+    )
+    args = parser.parse_args()
+
+    get_unlinked_top_containers(args.page_size)
