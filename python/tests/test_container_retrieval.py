3745b08f5f1637df666f2ddee0968a2d68136752
Henry Rosen <herosen@library.ucla.edu>
2026-05-19 15:32:39 -0700

    Fix failing test after refactor

--- python/tests/test_container_retrieval.py
+++ python/tests/test_container_retrieval.py
@@ -1,8 +1,8 @@
 import unittest
 from asnake.client import ASnakeClient
-from add_alma_barcodes_to_archivesspace import (
-    _get_container_refs_from_api,
-    _get_container_refs_from_db,
+from utils.aspace_utils import (
+    get_container_refs_from_api,
+    get_container_refs_from_db,
 )
 
 
@@ -14,8 +14,9 @@ class TestContainerRetrieval(unittest.TestCase):
         cls.db_settings = cls.aspace_client.config.get("database")
 
     def test_retrievals_are_identical(self):
+        repo_id = 2
         resource_id = 3002  # should have just a few top containers
-        api_refs = _get_container_refs_from_api(self.aspace_client, resource_id)
-        db_refs = _get_container_refs_from_db(self.db_settings, resource_id)
+        api_refs = get_container_refs_from_api(self.aspace_client, repo_id, resource_id)
+        db_refs = get_container_refs_from_db(self.db_settings, resource_id)
         # Don't check sizes as local data can change, but confirm the values are the same.
         self.assertEqual(api_refs, db_refs)
