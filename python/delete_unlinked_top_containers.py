9fcf2b83ef7d80fc4f9db44da2c7787d9744a4d9
Henry Rosen <herosen@library.ucla.edu>
2026-05-21 11:31:15 -0700

    Update `delete_unlinked_top_containers.py`
    - Move `ASnakeClient` instantiation to `main()` with config
    - Add `--dry_run` mode with logging

--- python/delete_unlinked_top_containers.py
+++ python/delete_unlinked_top_containers.py
@@ -4,14 +4,12 @@ import argparse
 
 from pathlib import Path
 
-from utils import configure_logging
+from utils import configure_logging, load_config
 
 # Logger available globally within this module.
 # Configuration is done by configure_logging(), which is called by main().
 # Made available globally so that tests can use the same logger with their own configuration.
 logger = logging.get_logger(Path(__file__).stem)
-
-client = ASnakeClient()
 
 
 def _get_args() -> argparse.Namespace:
@@ -21,13 +19,25 @@ def _get_args() -> argparse.Namespace:
     """
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "container_list_file",
+        "-f",
+        "--container_list_file",
         help="Path to a file containing a list of top container URIs to delete",
+    )
+    parser.add_argument(
+        "-c",
+        "--config_file",
+        help="Path to a YAML configuration file with ArchivesSpace credentials",
+        required=True,
+    )
+    parser.add_argument(
+        "--dry_run",
+        action="store_true",
+        help="Log intended deletions without updating ArchivesSpace.",
     )
     return parser.parse_args()
 
 
-def container_is_unlinked(top_container_uri: str) -> bool:
+def container_is_unlinked(client: ASnakeClient, top_container_uri: str) -> bool:
     """Checks if a top container is unlinked by looking foran empty collection field.
 
     :param str top_container_uri: The URI of the top container to check.
@@ -37,7 +47,7 @@ def container_is_unlinked(top_container_uri: str) -> bool:
     return len(top_container["collection"]) == 0
 
 
-def container_has_errors(top_container_uri: str) -> bool:
+def container_has_errors(client: ASnakeClient, top_container_uri: str) -> bool:
     """Checks if a top container has errors by looking for an error key in the response.
 
     :param str top_container_uri: The URI of the top container to check.
@@ -52,10 +62,14 @@ def container_has_errors(top_container_uri: str) -> bool:
     return False
 
 
-def delete_unlinked_top_containers(container_list_file: str):
+def delete_unlinked_top_containers(
+    client: ASnakeClient, container_list_file: str, dry_run: bool
+):
     """Deletes unlinked top containers from an ASpace repository.
 
+    :param ASnakeClient client: ASnake client instance.
     :param str container_list_file: Path to file containing URIs of top containers to delete.
+    :param bool dry_run: If True, log intended deletions without updating ArchivesSpace.
     """
     logger.info(f"Reading top container URIs to delete from {container_list_file}")
     with open(container_list_file, "r") as f:
@@ -65,46 +79,58 @@ def delete_unlinked_top_containers(container_list_file: str):
     deleted_count = 0
     skipped_uri_list = []
     for container in container_list:
-        if container_has_errors(container):
+        if container_has_errors(client, container):
             # error message already logged in container_has_errors()
             skipped_uri_list.append(container)
             continue
-        if container_is_unlinked(container):
-            logger.info(f"Deleting unlinked top container: {container}")
-            delete_response = client.delete(container)
-            status_code = delete_response.status_code
-            if status_code == 200:
-                # All OK
-                deleted_count += 1
-            elif status_code == 403:
-                # Forbidden
-                logger.error(
-                    f"Permission denied deleting top container {container}:"
-                    f"{delete_response.json()}"
-                )
-                raise PermissionError(
-                    f"Permission denied deleting top container {container}"
-                )
+        if container_is_unlinked(client, container):
+            if dry_run:
+                logger.info(f"DRY RUN: Would delete unlinked top container {container}")
+                continue
             else:
-                # Unknown error
-                logger.error(
-                    f"Unknown error {status_code} deleting top container {container}:"
-                    f"{delete_response.json()}"
-                )
+                logger.info(f"Deleting unlinked top container: {container}")
+                delete_response = client.delete(container)
+                status_code = delete_response.status_code
+                if status_code == 200:
+                    # All OK
+                    deleted_count += 1
+                elif status_code == 403:
+                    # Forbidden
+                    logger.error(
+                        f"Permission denied deleting top container {container}:"
+                        f"{delete_response.json()}"
+                    )
+                    raise PermissionError(
+                        f"Permission denied deleting top container {container}"
+                    )
+                else:
+                    # Unknown error
+                    logger.error(
+                        f"Unknown error {status_code} deleting top container {container}:"
+                        f"{delete_response.json()}"
+                    )
         else:
             logger.info(
                 f"Top container {container} is linked to a collection. Skipping deletion."
             )
             skipped_uri_list.append(container)
-    logger.info(f"Deleted {deleted_count} top containers")
-    logger.info(f"Skipped {len(skipped_uri_list)} top containers: {skipped_uri_list}")
+    logger.info(
+        f"{'DRY RUN: Would delete' if dry_run else 'Deleted'} {deleted_count} top containers"
+    )
+    logger.info(
+        f"{'DRY RUN: Would skip' if dry_run else 'Skipped'} "
+        f"{len(skipped_uri_list)} top containers: {skipped_uri_list}"
+    )
 
 
 def main() -> None:
     """Deletes unlinked top containers from an ASpace repository."""
     configure_logging(Path(__file__).stem)
     args = _get_args()
-    delete_unlinked_top_containers(args.container_list_file)
+    config = load_config(args.config_file)
+    client = ASnakeClient(**config)
+
+    delete_unlinked_top_containers(client, args.container_list_file, args.dry_run)
 
 
 if __name__ == "__main__":
