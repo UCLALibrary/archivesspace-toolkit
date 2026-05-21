2005d54dd1b7f247775a86c304d622cd850ae0c1
Henry Rosen <herosen@library.ucla.edu>
2026-05-21 09:00:05 -0700

    Fix bug related to log filename

--- python/add_alma_barcodes_to_archivesspace.py
+++ python/add_alma_barcodes_to_archivesspace.py
@@ -396,7 +396,7 @@ def main() -> None:
 
     # For convenience while debugging, print log name without full container path.
     # Also used in names of some output files.
-    logging_filename_base = Path(logging.handler.baseFilename).stem
+    logging_filename_base = Path(__file__).stem
     print(f"Logging to {logging_filename_base}.log")
     configure_logging(log_filename_stem=logging_filename_base)
 
