2c619f755e7027311f4d36e4bbfb896f7fff0bc5
Henry Rosen <herosen@library.ucla.edu>
2026-05-28 14:03:16 -0700

    Implement main logic for duplicate groups
    - Add `dry_run` mode

--- python/merge_duplicate_containers_aspace.py
+++ python/merge_duplicate_containers_aspace.py
@@ -2,6 +2,7 @@ import argparse
 from asnake.client import ASnakeClient
 from asnake.logging import get_logger
 from collections import defaultdict
+from copy import deepcopy
 from pathlib import Path
 
 from utils import configure_logging, load_config
@@ -34,6 +35,12 @@ def _get_args() -> argparse.Namespace:
         type=int,
         required=True,
         help="ArchivesSpace resource ID to process.",
+    )
+    parser.add_argument(
+        "-d",
+        "--dry_run",
+        action="store_true",
+        help="Run in dry run mode, without making any changes to ArchivesSpace.",
     )
     return parser.parse_args()
 
@@ -81,28 +88,74 @@ def _determine_canonical_tc(tcs: list[dict]) -> tuple[dict, list[dict]]:
     :param list[dict] tcs: List of top container records.
     :return: A tuple of the canonical top container and the remaining duplicate TCs.
     """
+    sorted_by_ao_count = sorted(tcs, key=lambda tc: len(tc["ao_refs"]), reverse=True)
+    return sorted_by_ao_count[0], sorted_by_ao_count[1:]
 
-    canonical_tc = max(tcs, key=_count_ao_refs_for_tc)
+
+def _update_archival_object(
+    aspace_client: ASnakeClient,
+    ao_ref: str,
+    ao_body: dict,
+    dry_run: bool,
+) -> None:
+    """Update an archival object in ArchivesSpace."""
+    if not dry_run:
+        aspace_client.post(ao_ref, json=ao_body).json()
+    logger.info(
+        f"{'DRY RUN: Would update' if dry_run else 'Updated'} "
+        f"instance(s) on archival object {ao_ref}"
+    )
+
+
+def _relink_archival_objects(
+    aspace_client: ASnakeClient,
+    canonical_tc: dict,
+    duplicate_tcs: list[dict],
+    dry_run: bool,
+) -> None:
+    """Relink archival objects from duplicates to the canonical top container."""
+    for duplicate_tc in duplicate_tcs:
+        for ao_ref in duplicate_tc["ao_refs"]:
+            archival_object = aspace_client.get(ao_ref).json()
+            original_instances = archival_object.get("instances", [])
+            if not original_instances:
+                continue
+            # Create a deep copy for comparison below
+            new_instances = deepcopy(original_instances)
+            for instance in new_instances:
+                sub_container = instance.get("sub_container", {})
+                top_container = sub_container.get("top_container", {})
+                # Overwrite the ref to the duplicate TC with the canonical TC
+                if top_container.get("ref", "") == duplicate_tc.get("uri"):
+                    instance["sub_container"]["top_container"]["ref"] = (
+                        canonical_tc.get("uri")
+                    )
+            # If the instances have changed, update the AO in ArchivesSpace,
+            # or log the intended update if dry_run
+            if original_instances != new_instances:
+                archival_object["instances"] = new_instances
+                _update_archival_object(aspace_client, ao_ref, archival_object, dry_run)
 
 
 def _merge_duplicate_containers(
     aspace_client: ASnakeClient,
     db_config: dict,
     resource_id: int,
+    dry_run: bool,
 ) -> None:
     """Merge duplicate top containers in ArchivesSpace."""
     tcs_by_indicator = _get_tcs_by_indicator(aspace_client, db_config, resource_id)
     # Find TC records that have duplicate (type, indicator) keys
-    duplicate_tcs: list[tuple[str, str, list[dict]]] = [
+    duplicate_groups: list[tuple[str, str, list[dict]]] = [
         (type, indicator, tcs)
         for (type, indicator), tcs in tcs_by_indicator.items()
         if len(tcs) > 1
     ]
-    if not duplicate_tcs:
+    if not duplicate_groups:
         logger.info("No duplicate top containers found for Resource ID {resource_id}.")
         return
 
-    for type, indicator, tcs in duplicate_tcs:
+    for type, indicator, tcs in duplicate_groups:
         logger.info(
             f"Found {len(tcs)} top containers "
             f"with type '{type}' and indicator '{indicator}'"
@@ -112,6 +165,10 @@ def _merge_duplicate_containers(
         for tc in tcs:
             tc_id = int(tc.get("uri", "0").split("/")[-1])
             tc["ao_refs"] = get_ao_refs_for_top_container_from_db(db_config, tc_id)
+
+        canonical_tc, duplicate_tcs = _determine_canonical_tc(tcs)
+
+        _relink_archival_objects(aspace_client, canonical_tc, duplicate_tcs, dry_run)
 
 
 def main() -> None:
@@ -134,7 +191,9 @@ def main() -> None:
         raise ValueError("DB connection settings are required.")
     aspace_client = ASnakeClient(**config)
 
-    _merge_duplicate_containers(aspace_client, db_config, args.resource_id)
+    _merge_duplicate_containers(
+        aspace_client, db_config, args.resource_id, args.dry_run
+    )
 
 
 if __name__ == "__main__":
